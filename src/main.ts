a443e6276ffc35b435773bfb9bf25358ae719adb
dominic <dominic@tweetworldtravel.com>
2024-09-26 12:32:12 +0700

    add swc

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,24 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SeedService } from './seed/seed.service';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const seedService = app.get(SeedService);
-  await seedService.seed();
+  // const seedService = app.get(SeedService);
+  // await seedService.seed();
   const configService = app.get(ConfigService);
-  await app.listen(configService.get<number>("port"));
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(configService.get<number>('port'));
   // await app.listen(3003);
 }
 bootstrap();
